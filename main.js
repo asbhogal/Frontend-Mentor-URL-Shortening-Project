71fbe898ca2cb5f7c55b0f37bcc199f3c0f62a46
ofthewildfire <lissajace@gmail.com>
2023-06-06 11:41:29 +0200

    HTML + CSS for mobile. 45% -ish.

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import "./src/styles/index.scss";
+
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+hamburgerMenu.addEventListener("click", function () {
+	console.log("clicked");
+	const navList = document.querySelector(".nav-list");
+	if (navList.classList.contains("hidden")) {
+		navList.classList.remove("hidden");
+	} else {
+		navList.classList.add("hidden");
+	}
+});
