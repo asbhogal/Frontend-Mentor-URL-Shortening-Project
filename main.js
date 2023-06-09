90e819fa536dc5f83f3d6f46d04188ef371e241d
ofthewildfire <lissajace@gmail.com>
2023-06-09 13:59:49 +0200

    API call | appending result to page.

--- main.js
+++ main.js
@@ -11,3 +11,29 @@ hamburgerMenu.addEventListener("click", function () {
 		navList.classList.add("hidden");
 	}
 });
+
+// The API stuff -- lets play! Be kind to me computer.
+
+async function callAPI() {
+	const inputURL = document.querySelector(".link-input").value;
+	const container = document.querySelector(".shortend-urls-container");
+	try {
+		const response = await fetch(
+			`https://api.shrtco.de/v2/shorten?url=${inputURL}/`
+		);
+		const data = await response.json();
+		const urlLink = data.result.short_link;
+		container.innerHTML += `
+		<div class="shortend-url">
+		<h2 class="url">${urlLink}</h2>
+	</div>
+		`;
+	} catch (err) {
+		err = "Sorry, not sure what happened there!";
+		console.log(err);
+	}
+}
+
+const shortenLinkBtn = document.querySelector(".shorten-link-btn");
+
+shortenLinkBtn.addEventListener("click", callAPI);
