c2e15af8895ad81fee493162a32c25e4079864e4
ofthewildfire <lissajace@gmail.com>
2023-06-11 12:11:40 +0200

    JS testings, adding of CSS to boxes for links.

--- main.js
+++ main.js
@@ -25,14 +25,34 @@ async function callAPI() {
 		const urlLink = data.result.short_link;
 		container.innerHTML += `
 		<div class="shortend-url">
-		<h2 class="url">${urlLink}</h2>
-	</div>
-		`;
+			<h2 class="input">${inputURL}</h2>
+			<h2 class="url">${urlLink}</h2>
+			<button class="btn copy-btn"> Copy It </button>
+		</div>
+			`;
 	} catch (err) {
 		err = "Sorry, not sure what happened there!";
 		console.log(err);
 	}
 }
+
+// Honestly dont like this solution. But, hmm, lets just get her done, and then come back and refactor, apply a different solution.
+
+// document
+// 	.querySelector(".shortend-urls-container")
+// 	.addEventListener("click", (e) => {
+// 		e.preventDefault();
+// 		e.target.addEventListener("click", () => {
+// 			// #PANDEMONIUM
+// 			console.log(e.target);
+// 			document.querySelector(".copy-btn").classList.add("copied--confirm");
+// 		});
+// 	});
+
+// Hot garbage!
+
+// Clipboard API + testings.
+//Holy heck; its an API /facepalm it works with a promise too... ah ofc. Read more before you start again. X~
 
 const shortenLinkBtn = document.querySelector(".shorten-link-btn");
 
