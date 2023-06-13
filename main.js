e0aa35a0f412b2b086752abcdfa0c1425ef5a4f8
ofthewildfire <lissajace@gmail.com>
2023-06-13 06:21:08 +0200

    added working copy text to clipboard

--- main.js
+++ main.js
@@ -1,59 +1,68 @@
 import "./src/styles/index.scss";
 
 const hamburgerMenu = document.querySelector(".hamburger-menu");
+const shortenLinkBtn = document.querySelector(".shorten-link-btn");
 
 hamburgerMenu.addEventListener("click", function () {
-  console.log("clicked");
-  const navList = document.querySelector(".nav-list");
-  if (navList.classList.contains("hidden")) {
-    navList.classList.remove("hidden");
-  } else {
-    navList.classList.add("hidden");
-  }
+	console.log("clicked");
+	const navList = document.querySelector(".nav-list");
+	if (navList.classList.contains("hidden")) {
+		navList.classList.remove("hidden");
+	} else {
+		navList.classList.add("hidden");
+	}
 });
 
-// The API stuff -- lets play! Be kind to me computer.
+// The API.
 
 async function callAPI() {
-  const inputURL = document.querySelector(".link-input").value;
-  const container = document.querySelector(".shortend-urls-container");
-  try {
-    const response = await fetch(
-      `https://api.shrtco.de/v2/shorten?url=${inputURL}/`
-    );
-    const data = await response.json();
-    const urlLink = data.result.short_link;
-    container.innerHTML += `
+	const inputURL = document.querySelector(".link-input").value;
+	const container = document.querySelector(".shortend-urls-container");
+	try {
+		const response = await fetch(
+			`https://api.shrtco.de/v2/shorten?url=${inputURL}/`
+		);
+		const data = await response.json();
+		const urlLink = data.result.short_link;
+
+		container.innerHTML += `
 		<div class="shortend-url">
 			<h2 class="input">${inputURL}</h2>
 			<h2 class="url">${urlLink}</h2>
-			<button class="btn copy-btn"> Copy It </button>
+			<button class="btn copy-btn"> 
+			Copy It 
+			</button>
 		</div>
 			`;
-  } catch (err) {
-    err = "Sorry, not sure what happened there!";
-    console.log(err);
-  }
+	} catch (err) {
+		err = "Sorry, not sure what happened there!";
+		console.log(err);
+	}
+	// Clear the input box
+	document.querySelector(".link-input").value = "";
 }
 
-// Honestly dont like this solution. But, hmm, lets just get her done, and then come back and refactor, apply a different solution.
-
-// document
-// 	.querySelector(".shortend-urls-container")
-// 	.addEventListener("click", (e) => {
-// 		e.preventDefault();
-// 		e.target.addEventListener("click", () => {
-// 			// #PANDEMONIUM
-// 			console.log(e.target);
-// 			document.querySelector(".copy-btn").classList.add("copied--confirm");
-// 		});
-// 	});
-
-// Hot garbage!
-
-// Clipboard API + testings.
-//Holy heck; its an API /facepalm it works with a promise too... ah ofc. Read more before you start again. X~
-
-const shortenLinkBtn = document.querySelector(".shorten-link-btn");
+// Clipboard API
 
 shortenLinkBtn.addEventListener("click", callAPI);
+
+// Text copying.
+const urlBoxContainer = document.querySelector(".shortend-urls-container");
+
+urlBoxContainer.addEventListener("click", (event) => {
+	const clicked = event.target;
+	const urlToCopy = clicked.previousElementSibling.innerText;
+
+	if (clicked.classList.contains("copy-btn")) {
+		navigator.clipboard
+			.writeText(urlToCopy)
+			.then(() => {
+				// Temp styling and changed on copy, its 6-am and my brain is just not having this "sleep deprivation" anymore. But, I am happy it works. Will work on it more.
+				clicked.innerText = "Copied";
+				clicked.style.backgroundColor = "purple";
+			})
+			.catch((err) => {
+				console.error(`Error copying text to clipboard: ${err}`);
+			});
+	}
+});
