81980e35e9005a83b80fed972695617cd60fd5b8
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-06-25 19:37:42 +0100

    added comment to vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -6,3 +6,5 @@ export default defineConfig({
     devSourcemap: true,
   },
 });
+
+/// the vite config file allows you to make custom config options to the build. Here, I've added source mapping for CSS, which means in dev tools, we can see the file and line number where certain styles for elements are - will send you a linkie
